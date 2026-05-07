deb395f2e994471de312400d99965c5b0123983c
leop4-cod <leo0411201@gmail.com>
2026-05-07 11:00:24 -0500

    CLASE 7/5/2026

--- modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
+++ modulo_2/compose/app/src/main/java/com/ute/compose/MainActivity.kt
@@ -7,7 +7,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import com.ute.compose.ui.*
 import com.ute.compose.ui.material3.Paso01SumaScreen
-import com.ute.compose.ui.material3.Paso01TextFieldScreen
+import com.ute.compose.ui.material3.Paso02CardScreen
+import com.ute.compose.ui.material3.Paso02CompraScreen
+import com.ute.compose.ui.material3.Paso03LazyColumnScreen
+import com.ute.compose.ui.material3.Paso04ScaffoldScreen
+import com.ute.compose.ui.material3.Paso05NavBarScreen
+import com.ute.compose.ui.material3.Paso06DialogosScreen
+//import com.ute.compose.ui.material3.Paso01TextFieldScreen
 import com.ute.compose.ui.screens.S02_TextScreen
 import com.ute.compose.ui.screens.S03ButtonScreen
 import com.ute.compose.ui.screens.S04LayoutScreen
@@ -23,12 +29,15 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 // ◀ CAMBIA AQUÍ para probar cada paso:
                 //Paso01TextFieldScreen()
-                Paso01SumaScreen()
+                //Paso01SumaScreen()
+                //Paso02CompraScreen()
+                //Paso02CardScreen()
+               // Paso03LazyColumnScreen()
                 // Paso02_CardScreen()
                 // Paso03_LazyColumnScreen()
-                // Paso04_ScaffoldScreen()
-                // Paso05_NavBarScreen()
-                //Paso06_DialogosScreen()   // ← paso activo
+                 //Paso04ScaffoldScreen()
+                // Paso05NavBarScreen()
+                Paso06DialogosScreen()   // ← paso activo
             }
         }
     }
